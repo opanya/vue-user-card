0a905cb77b7dab396bd8921329d28165ff9af658
Andrey <golgus17@gmail.com>
2022-12-14 19:37:22 +0300

    Add new vue file

---
+++ src/components/UserCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <UserCard></UserCard>
+</template>
+
+<script>
+export default {
+    name: 'UserCard',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style>
+
+</style>
